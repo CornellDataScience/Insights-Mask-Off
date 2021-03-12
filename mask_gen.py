5b2609fd6811ee5f76a7a2efbd6754db8b671933
Kaitlyn Chen <kjc256@cornell.edu>
2021-03-11 20:49:52 -0500

    put masks on George Bush

--- mask_gen.py
+++ mask_gen.py
@@ -7,44 +7,63 @@ Requires Python 3.5 (or older)
 import cv2
 import dlib
 import numpy as np
+import os
+from PIL import Image
 
-# Initial Setup
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-img = cv2.imread("test.png")
-gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
-face = detector(gray)[0]
-landmarks = predictor(image = gray, box = face)
 
-# Obtain points of interest
-right_jaw_x = landmarks.part(3).x
-right_jaw_y = landmarks.part(3).y
+def generate_mask(face, count):
+    # Initial Setup
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    print(face)
+    img = cv2.imread(face)
+    gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+    face = detector(gray)[0]
+    landmarks = predictor(image=img, box=face)
 
-bot_chin_x = landmarks.part(8).x
-bot_chin_y = landmarks.part(8).y
+    # Obtain points of interest
+    right_jaw_x = landmarks.part(3).x
+    right_jaw_y = landmarks.part(3).y
 
-left_jaw_x = landmarks.part(13).x
-left_jaw_y = landmarks.part(13).y
+    bot_chin_x = landmarks.part(8).x
+    bot_chin_y = landmarks.part(8).y
 
-middle_nose_x = landmarks.part(29).x
-middle_nose_y = landmarks.part(29).y
+    left_jaw_x = landmarks.part(13).x
+    left_jaw_y = landmarks.part(13).y
 
-major_axis_len = left_jaw_x - right_jaw_x
-minor_axis_len = bot_chin_y - middle_nose_y
+    middle_nose_x = landmarks.part(29).x
+    middle_nose_y = landmarks.part(29).y
 
-#cv2.ellipse(img,(middle_nose_x, middle_nose_y), 
-#                (major_axis_len, minor_axis_len),0,0,180,255,-1)
+    major_axis_len = left_jaw_x - right_jaw_x
+    minor_axis_len = bot_chin_y - middle_nose_y
 
-curve_face = []
-for i in range(1, 16):
-    curve_face.append([landmarks.part(i).x, landmarks.part(i).y])
+    # cv2.ellipse(img,(middle_nose_x, middle_nose_y),
+    #                (major_axis_len, minor_axis_len),0,0,180,255,-1)
 
-curve_face.append([middle_nose_x, middle_nose_y])
+    curve_face = []
+    for i in range(1, 16):
+        curve_face.append([landmarks.part(i).x, landmarks.part(i).y])
 
-#pts = np.array([[100,350],[165,350],[165,240]], np.int32)
-pts = np.array(curve_face, np.int32)
-cv2.fillPoly(img, [pts], 255)
+    curve_face.append([middle_nose_x, middle_nose_y])
 
-cv2.imshow(winname = "Face", mat = img)
-cv2.waitKey(delay = 0)
-cv2.destroyAllWindows()
+    #pts = np.array([[100,350],[165,350],[165,240]], np.int32)
+    pts = np.array(curve_face, np.int32)
+    mask = cv2.fillPoly(img, [pts], 255)
+
+    # cv2.imshow(winname="Face", mat=mask)
+    cv2.imwrite('data/' + 'George_W_Bush_' + str(count) + '_m.png', mask)
+    cv2.waitKey(delay=0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    try:
+        # creating a folder named data
+        if not os.path.exists('data'):
+            os.makedirs('data')
+        # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
+
+    for i in range(1, 21):
+        generate_mask('George_W_Bush/George_W_Bush_' + str(i) + '.png', i)
