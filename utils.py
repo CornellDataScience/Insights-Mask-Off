fe786550fc3eb89c1cb750bd4d056ccefec79a96
Greg <gbodik416@gmail.com>
2021-04-27 22:29:13 -0400

    fixed code to properly display losses graph (and run without error)

---
+++ utils.py
@@ -0,0 +1,151 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader,Dataset
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import torchvision
+import torchvision.transforms as transforms
+
+
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            
+            nn.Conv2d(1, 96, kernel_size=11,stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(3, stride=2),
+            
+            nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+
+            nn.Conv2d(256,384 , kernel_size=3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(384,256 , kernel_size=3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+        
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(30976, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+            
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(128,2))
+
+    def forward_once(self, x):
+        # Forward pass 
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on:
+    """
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, x0, x1, y):
+        # euclidian distance
+        diff = x0 - x1
+        dist_sq = torch.sum(torch.pow(diff, 2), 1)
+        dist = torch.sqrt(dist_sq)
+
+        mdist = self.margin - dist
+        dist = torch.clamp(mdist, min=0.0)
+        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
+        loss = torch.sum(loss) / 2.0 / x0.size()[0]
+        return loss
+
+class SiameseDataset():
+    def __init__(self,training_csv=None,training_dir=None,transform=None):
+        # used to prepare the labels and images path
+        self.train_df=pd.read_csv(training_csv)
+        self.train_df.columns =["image1","image2","label"]
+        self.train_dir = training_dir    
+        self.transform = transform
+
+    def __getitem__(self,index):
+        # getting the image path
+        image1_path=os.path.join(self.train_dir,self.train_df.iat[index,0])
+        image2_path=os.path.join(self.train_dir,self.train_df.iat[index,1])
+        # Loading the image
+        img0 = Image.open(image1_path)
+        img1 = Image.open(image2_path)
+        img0 = img0.convert("L")
+        img1 = img1.convert("L")
+        # Apply image transformations
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        return img0, img1 , torch.from_numpy(np.array([int(self.train_df.iat[index,2])],dtype=np.float32))
+    def __len__(self):
+        return len(self.train_df)
+
+def show_plot(iteration,loss):
+    import matplotlib.pyplot as plt 
+    import os
+    os.environ['QT_QPA_PLATFORM']='offscreen'
+    plt.plot(iteration,loss)
+    plt.savefig('new_1.png')
+
+def imshow(img,text=None,should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic',fontweight='bold',
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# Train the model
+def train(train_dataloader, optimizer, criterion, net):
+    loss=[] 
+    counter=[]
+    iteration_number = 0
+    for epoch in range(1,21): #config.epochs):
+        for i, data in enumerate(train_dataloader,0):
+            img0, img1 , label = data
+            img0, img1 , label = img0, img1, label
+            optimizer.zero_grad()
+            output1,output2 = net(img0,img1)
+            loss_contrastive = criterion(output1,output2,label)
+            loss_contrastive.backward()
+            optimizer.step()
+        print("Epoch {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
+        iteration_number += 1
+        counter.append(iteration_number)
+        loss.append(loss_contrastive.item())
+    show_plot(counter, loss) 
+    print(counter)
+    print()
+    print(loss)
+    return net
